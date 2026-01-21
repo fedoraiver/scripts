61fbb63ef44338693d97949876e455578d3721b0
fedoraiver <cck1772416022@gmail.com>
2026-01-21 19:42:26 +0800

    better

--- vllm/api.py
+++ vllm/api.py
@@ -1,20 +1,19 @@
 from openai import OpenAI
 
 client = OpenAI(
-    base_url="http://localhost:8001/v1",
+    base_url="http://localhost:8002/v1",
     api_key="EMPTY",
 )
 
 resp = client.chat.completions.create(
-    model="/mnt/nvme0/tdy/my_models/HY/",
+    model="HY",
     messages=[
         {
             "role": "system",
             "content": "Translate the following segment into Spanish, without additional explanation.\n\n",
         },
-        {"role": "user", "content": "The future of AI is bright."},
+        {"role": "user", "content": "今天中午吃包子."},
     ],
-    # 👇 vLLM 扩展参数统一放这里
     extra_body={
         "max_tokens": 32,
         "top_k": 20,
